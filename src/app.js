dd37e2ffb07633d2fb659f7772e4ffb9eac23a84
Ashraf Shahin <ashraf.hossein.web@gmail.com>
2026-06-06 20:37:18 +0100

    General Rate Limiter for all APIs ... v-14,15 er kichu

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const dbConfig = require('./config/dbConfig');
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpecs = require('./config/swagger');
+const { apiLimiter } = require('./middlewares/rateLimiter');
 
 
 const app = express()
@@ -24,6 +25,7 @@ app.use(cookieParser())
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs) )
 
 // Routes 
+app.use('/api/v1/', apiLimiter)
 app.use('/api/v1/auth', authRoutes)
 
 // Database configaration...
--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -34,11 +34,20 @@ const refreshTokenLimiter = rateLimit({
     legacyHeaders: false,
 });
 
-
+const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // 100 request per IP every 15 minutes
+    message: {
+        success: false,
+        message: 'Too many attemps, Please try again after 15 miutes...'
+    },
+    standardHeaders: true, // it will return Reg Rate Limit Info in Headers...
+    legacyHeaders: false,
+});
 
 module.exports = {
     registerLimiter,
     loginLimiter,
     refreshTokenLimiter,
-    
+    apiLimiter,
 }
